d5ed6f2a9ec31744bd2da04c1d97a01bb783ee85
Rubens Abacherli <rubensabacherli@gmail.com>
2024-10-24 21:57:34 -0300

    comitando algumas alterações do código

--- contact/admin.py
+++ contact/admin.py
@@ -5,3 +5,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_max_show_all = 200
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
